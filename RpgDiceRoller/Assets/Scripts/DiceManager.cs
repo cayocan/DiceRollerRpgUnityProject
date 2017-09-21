10ca92cd09fc5dfbb2ac47a555c494ea52587a4d
DESKTOP-2QF32HE\cayoc <cayocan@hotmail.com>
2017-09-21 10:02:21 -0300

    Maioria das funcionalidades implementadas

---
+++ RpgDiceRoller/Assets/Scripts/DiceManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceManager : MonoBehaviour {
+    public static DiceMode mode;
+    public Text displayScreen;
+
+    private void Awake()
+    {
+        mode = DiceMode.SingleMode;
+    }
+
+    public enum DiceMode
+    {
+        SumMode,
+        SingleMode
+    };
+
+    public void RollDice(int diceType)
+    {
+        if (mode == DiceMode.SingleMode)
+        {
+            displayScreen.text = Random.Range(1, diceType + 1).ToString();
+        }
+    }
+
+    public void HeadsOrTails()
+    {
+        string[] faces = new string[2];
+
+        faces[0] = "Head";
+        faces[1] = "Tail";
+
+        displayScreen.text = faces[Random.Range(0, 2)];
+    }
+
+    public void ClearDisplay()
+    {
+        displayScreen.text = "";
+    }
+}
